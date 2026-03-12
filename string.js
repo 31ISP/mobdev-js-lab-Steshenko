1c504e86708919924256cca5440e870941053495
Artem <101925080+supersuss@users.noreply.github.com>
2026-03-12 10:47:37 +0000

    rabota 2

--- string.js
+++ string.js
@@ -1,3 +1,12 @@
 const sentence = "   javascript - Это просто!   "
+const name = "Артём"
+const gruppa = "31ИСП"
+const ball = "4.75"
+const date = "2025-03-15"
+const text = "Node.js разработчик"
 console.log(sentence.trim().toUpperCase()[0] + sentence.trim().slice(1))
-console.log()
+console.log(`Студент Стешенко ${name}, группа:${gruppa}, средний балл: ${ball}`)
+const datanash = date.split('-')
+console.log(`${datanash[2]}.${datanash[1]}.${datanash[0]}`)
+console.log(text.startsWith("Node"),text.endsWith("разработчик"))
+console.log("42".padStart(8, "0"))
